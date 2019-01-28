e14157984e4d60b932a0cd9cc19286d2088da14a
Carissa Wagner <carissawagner@Carissas-MacBook-Air.local>
2019-01-28 11:49:28 -0800

    Creates a view of all the resources

--- techreview/tech/views.py
+++ techreview/tech/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import ProductType
 
 # Create your views here.
 
 def index(request):
     return render(request, 'tech/index.html')
+
+def techtypes(request):
+    type_list = ProductType.objects.all()
+    return render(request, 'tech/types.html', {'type_list': type_list})
